21204a9e77de00b13970f3023faadb2a98cb2020
Alexander Molchan <79983103+AlexanderMolchan@users.noreply.github.com>
2023-01-17 19:22:52 +0300

    TabBarCreatingCommit

---
+++ BankApp/Controllers/MapController/MapController.swift
@@ -0,0 +1,17 @@
+//
+//  MapController.swift
+//  BankApp
+//
+//  Created by Александр Молчан on 17.01.23.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
