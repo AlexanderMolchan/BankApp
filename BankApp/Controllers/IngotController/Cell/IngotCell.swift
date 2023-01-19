59fd70c8cc652a81bdb0519c37703bf15e8a4612
Alexander Molchan <79983103+AlexanderMolchan@users.noreply.github.com>
2023-01-19 21:54:12 +0300

    TableViewsCreated

---
+++ BankApp/Controllers/IngotController/Cell/IngotCell.swift
@@ -0,0 +1,54 @@
+//
+//  IngotCell.swift
+//  BankApp
+//
+//  Created by Александр Молчан on 19.01.23.
+//
+
+import UIKit
+
+class IngotCell: UITableViewCell {
+    static let id = String(describing: IngotCell.self)
+    
+    @IBOutlet weak var filialLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var tenLabel: UILabel!
+    @IBOutlet weak var twentyLabel: UILabel!
+    @IBOutlet weak var fiftyLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func set(model: IngotModel, type: IngotType) {
+        filialLabel.text = "\(model.city), отделение №\(model.filialNumber)"
+        var tenCost = ""
+        var twentyCost = ""
+        var fiftyCost = ""
+        switch type {
+            case .gold:
+                tenCost = model.goldTen
+                twentyCost = model.goldTwenty
+                fiftyCost = model.goldFifty
+            case .silver:
+                tenCost = model.silverTen
+                twentyCost = model.silverTwenty
+                fiftyCost = model.silverFifty
+            case .platinum:
+                tenCost = model.platinumTen
+                twentyCost = model.platinumTwenty
+                fiftyCost = model.platinumFifty
+        }
+        typeLabel.text = type.rawValue
+        tenLabel.text = "Цена за слиток 10г. \(tenCost)"
+        twentyLabel.text = "Цена за слиток 20г. \(twentyCost)"
+        fiftyLabel.text = "Цена за слиток 50г. \(fiftyCost)"
+    }
+    
+}
+
+enum IngotType: String {
+    case silver = "Серебро"
+    case gold = "Золото"
+    case platinum = "Платина"
+}
